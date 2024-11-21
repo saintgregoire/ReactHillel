383422c04763d627f77b645cde515f1101c15732
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-11-21 12:51:55 +0100

    feat=> (PizzaItem) => counter added

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,29 +1,78 @@
+import { useState } from "react";
+
 import "./PizzaItem.css";
 
 function PizzaItem(props) {
+  const { data } = props;
+  const { name, imageUrl, ingredients, unitPrice, soldOut } = data;
 
-  const {data} = props;
-  const {name, imageUrl, ingredients, unitPrice, soldOut} = data;
+  const [isFlex, setIsFlex] = useState(false);
+  const [isBlock, setIsBlock] = useState(true);
+  const [count, setCount] = useState(1);
+
+  const handleClickShowCounter = () => {
+    setIsBlock(false);
+    setIsFlex(true);
+  };
+
+  const decrementCounter = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      setIsFlex(false);
+      setIsBlock(true);
+    }
+  };
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div className="pizza-item">
-      <img
-        src={imageUrl}
-        alt={`${name} Pizza`}
-        className="pizza-image"
-      />
+      <img src={imageUrl} alt={`${name} Pizza`} className="pizza-image" />
       <div className="pizza-info">
         <h2>{name}</h2>
-        <p className="ingredients">{ingredients.join(', ')}</p>
+        <p className="ingredients">{ingredients.join(", ")}</p>
         {!soldOut ? (
           <p className="price">{`€${unitPrice}.00`}</p>
         ) : (
           <p className="sold-out">SOLD OUT</p>
         )}
       </div>
-      {!soldOut && <button className="add-to-cart">ADD TO CART</button>}
+      {!soldOut && (
+        <div className="cart-controls">
+          <button
+            className="add-to-cart"
+            style={{ display: isBlock ? "block" : "none" }}
+            onClick={handleClickShowCounter}
+          >
+            ADD TO CART
+          </button>
+          <div
+            className="counter"
+            style={{ display: isFlex ? "flex" : "none" }}
+          >
+            <button
+              className="decrement"
+              aria-label="Decrease quantity"
+              onClick={decrementCounter}
+            >
+              -
+            </button>
+            <span>{count}</span>
+            <button
+              className="increment"
+              aria-label="Increase quantity"
+              onClick={incrementCounter}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-export default PizzaItem
+export default PizzaItem;
