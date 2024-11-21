b995ade85631a7340cc50196d9096fddb1c74158
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-11-21 13:38:51 +0100

    feat => (Button) => props added (Input) => props added (Home) => function for showing input value added

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,12 +1,22 @@
-import './Input.css';
+import "./Input.css";
 
 function Input(props) {
+  const { type = "text", placeholder, ariaLabel, className, value, onChange } = props;
 
-  const {type="text", placeholder, ariaLabel, className} = props;
+  const handleChangeInputValue = (e) => {
+    setValue(e.target.value);
+  };
 
   return (
-    <input type={type} placeholder={placeholder} aria-label={ariaLabel} className={className}></input>
-  )
+    <input
+      type={type}
+      placeholder={placeholder}
+      aria-label={ariaLabel}
+      className={className}
+      value={value}
+      onChange={onChange}
+    ></input>
+  );
 }
 
-export default Input
+export default Input;
