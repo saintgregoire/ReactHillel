7af01c5d3a742af65bc5847cfde6f9ab5f08b647
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-12-15 16:24:29 +0100

    feat => search input added to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import Input from "../Input/Input";
 import "./Header.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NameContext } from "../../context/NameContext";
 
 function Header() {
+  const { userName } = useContext(NameContext);
+  const [inputValue, setInputValue] = useState('');
+  const navigate = useNavigate();
 
-  const {userName} = useContext(NameContext);
+  const changeInputValue = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/orders/${inputValue}`);
+    setInputValue('');
+  }
 
   return (
     <header>
@@ -14,16 +25,26 @@ function Header() {
         <NavLink to="/" className="logo">
           PIZZA DAY
         </NavLink>
-        <NavLink to="/menu" className="logo">Menu</NavLink>
-        <NavLink to="/cart" className="logo">Cart</NavLink>
-        <NavLink to="/order_form" className="logo">Order Form</NavLink>
-        <NavLink to="/orders/5T460L" className="logo">Order Status</NavLink>
+        <NavLink to="/menu" className="logo">
+          Menu
+        </NavLink>
+        <NavLink to="/cart" className="logo">
+          Cart
+        </NavLink>
+        <NavLink to="/order_form" className="logo">
+          Order Form
+        </NavLink>
+        {/* <NavLink to="/orders/5T460L" className="logo">Order Status</NavLink> */}
       </nav>
-      <Input
-        type="text"
-        className="search-bar"
-        placeholder="Search for the order #"
-      />
+      <form onSubmit={formSubmit}>
+        <Input
+          type="text"
+          className="search-bar"
+          placeholder="Search for the order #"
+          value={inputValue}
+          onChange={changeInputValue}
+        />
+      </form>
       <div className="username">{userName}</div>
     </header>
   );
