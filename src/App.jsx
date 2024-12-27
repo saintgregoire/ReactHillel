460319140e1c0e96d3307a3542a16c092fd85b16
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-12-27 15:22:25 +0100

    feat => lazy + suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
 import Header from "./components/Header/Header";
-import Home from "./pages/Home/Home";
-import Menu from "./pages/Menu/Menu";
-import Cart from "./pages/Cart/Cart";
 import "./App.css";
 import { Route, Routes } from "react-router";
-import OrderForm from "./pages/OrderForm/OrderForm";
-import OrderStatus from "./pages/OrderStatus/OrderStatus";
 import CartWidget from "./components/CartVidget/CartWidget";
+import { lazy, Suspense } from "react";
+
+const HomeLazy = lazy(() => import("./pages/Home/Home"));
+const MenuLazy = lazy(() => import("./pages/Menu/Menu"));
+const CartLazy = lazy(() => import("./pages/Cart/Cart"));
+const OrderFormLazy = lazy(() => import("./pages/OrderForm/OrderForm"));
+const OrderStatusLazy = lazy(() => import("./pages/OrderStatus/OrderStatus"));
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order_form" element={<OrderForm/>} />
-        <Route path="/orders/:id" element={<OrderStatus/>} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <HomeLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/menu"
+          element={
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <MenuLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <CartLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/order_form"
+          element={
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <OrderFormLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/orders/:id"
+          element={
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <OrderStatusLazy />
+            </Suspense>
+          }
+        />
       </Routes>
       <CartWidget />
     </div>
